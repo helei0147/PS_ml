1e4361af39ebadd8f0b5bbca690cc4fc0d922664
URefrain <helei0147@163.com>
2017-09-11 14:04:07 +0800

    split picture into 3 channels

--- read_data.py
+++ read_data.py
@@ -1,13 +1,17 @@
 import os,sys
 import numpy as np
 
-def main(_):
-    train_observations,train_normals = read_data('data/train/')
-    test_observations, test_normals = read_data('data/test/')
+def main():
+    train_channel1, train_channel2, train_channel3, train_normals = read_data('data/train/')
+    test_channel1, test_channel2, test_channel3, test_normals = read_data('data/test/')
     # split training data into validation data and training data
-    np.save('train_observations.npy', train_observations);
-    np.save('train_normals.npy',train_normals);
-    np.save('test_observations.npy',test_observations);
+    np.save('train_channel1.npy', train_channel1);
+    np.save('train_channel2.npy', train_channel2);
+    np.save('train_channel3.npy', train_channel3);
+    np.save('train_normals.npy', train_normals);
+    np.save('test_channel1.npy', test_channel1);
+    np.save('test_channel2.npy', test_channel2);
+    np.save('test_channel3.npy', test_channel3);
     np.save('test_normals.npy',test_normals);
 
 def read_data(directory):
@@ -18,26 +22,38 @@ def read_data(directory):
     '''
     npy_files = os.listdir(directory+'observations/')
     normal_dict = read_all_normals()
-    observation_collection = []
+    channel1_collection = []
+    channel2_collection = []
+    channel3_collection = []
     normal_collection = []
     for filename in npy_files:
+        print(filename)
         model_index = int(filename.split('_')[0])
         normals = normal_dict[model_index]
+        temp = np.load(directory+'observations/'+filename)
+        temp_channel1 = temp[0::3,:]
+        temp_channel2 = temp[1::3,:]
+        temp_channel3 = temp[2::3,:]
+        assert temp_channel1.shape[0] == normals.shape[0],("ob:%s nm:%s"%(temp.shape,normals.shape))
+        if channel1_collection == []:
+            channel1_collection = temp_channel1
+            channel2_collection = temp_channel2
+            channel3_collection = temp_channel3
+        else:
+            channel1_collection = np.concatenate((channel1_collection, temp_channel1))
+            channel2_collection = np.concatenate((channel2_collection, temp_channel2))
+            channel3_collection = np.concatenate((channel3_collection, temp_channel3))
         if normal_collection == []:
             normal_collection = normals
         else:
-            normal_collection = np.concatenate((normal_collection, normals))
-        temp = np.load(directory+'observations/'+filename)
-        if observation_collection == []:
-            observation_collection = temp
-        else:
-            observation_collection = np.concatenate( (observation_collection, temp) )
-    return observation_collection,normal_collection
+            normal_collection = np.concatenate( (normal_collection, normals) )
+    return channel1_collection,channel2_collection,channel3_collection,normal_collection
 def read_all_normals():
     normal_collection = []
     for i in range(10):
-        normal_filename = '../normal/normal'+str(i)'.txt'
+        normal_filename = '../normal/normal'+str(i)+'.txt'
         normals = read_normal_to_array(normal_filename)
+        print(normals.shape[0])
         normal_collection.append(normals)
     return normal_collection
 def read_normal_to_array(normal_filename):
@@ -53,5 +69,5 @@ def read_normal_to_array(normal_filename):
         n_nums.append(float(string))
     normals = np.reshape(n_nums,(-1,3))
     return normals
-if '__name__' == '__main__':
+if __name__ == '__main__':
     main()
