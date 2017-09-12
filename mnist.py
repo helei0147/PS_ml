60d2bc3411384aac9a3658c537c6abdfaafeb889
URefrain <helei0147@163.com>
2017-09-12 15:59:53 +0800

    fix optimizer to Adam

--- mnist.py
+++ mnist.py
@@ -135,12 +135,8 @@ def training(loss, learning_rate):
     tf.summary.scalar(loss.op.name, loss)
 
     # Create the gradient descent optimizer with the given learning rate.
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    # Create a variable to track the global step.
-    global_step = tf.Variable(0, name='global_step', trainable=False)
-    # Use the optimizer to apply the gradients that minimize the loss
-    # (and also increment the global step counter) as a single training step.
-    train_op = optimizer.minimize(loss, global_step=global_step)
+    optimizer = tf.train.AdamOptimizer(1e-4)
+    train_op = optimizer.minimize(loss)
     return train_op
 
 
