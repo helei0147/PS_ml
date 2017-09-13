4f23ecf92ed7cca760f3ba91c159a87a14b12052
URefrain <helei0147@163.com>
2017-09-13 09:28:23 +0800

    .

--- fully_connected_feed.py
+++ fully_connected_feed.py
@@ -125,7 +125,7 @@ def run_training():
   """Train MNIST for a number of steps."""
   # Get the sets of images and labels for training, validation, and
   # test on MNIST.
-  data_sets = input_data.read_data_sets(FLAGS.train_channel_index, FLAGS.fake_data)
+  data_sets = input_data.read_data_sets(FLAGS.train_channel_index)
 
   # Tell TensorFlow that the model will be built into the default Graph.
   with tf.Graph().as_default():
